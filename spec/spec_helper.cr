9ce3f8ad995089b75f0d03550eadf0814281c0f0
Akash Manohar <akash@akash.im>
2022-10-14 05:57:04 -0400

    Use env var to pick manifest file path
    Signed-off-by: Akash Manohar <akash@akash.im>

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,3 +2,7 @@ require "spec"
 require "colorize"
 
 ENV["CRYSTAL_ENV"] = "TEST"
+
+# This forces use of cluster-tools manifest in the repo's tools dir when running specs.
+ENV["USE_MANIFEST_FOR_SPECS"] = "yes"
+
--- src/utils/embedded_file_manager.cr
+++ src/utils/embedded_file_manager.cr
@@ -1,5 +1,5 @@
-module EmbeddedFileManager 
+module EmbeddedFileManager
   macro cluster_tools
-    CLUSTER_TOOLS = Base64.decode_string("{{ `cat ./tools/cluster-tools/manifest.yml | base64` }}")
+    CLUSTER_TOOLS = ENV["USE_MANIFEST_FOR_SPECS"]? ? File.read("./tools/cluster-tools/manifest.yml") : File.read("./lib/cluster_tools/tools/cluster-tools/manifest.yml")
   end
 end
