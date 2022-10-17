bf46fb68c89e82ccb9dcb0facc8c26ddc130fee5
Akash Manohar <akash@akash.im>
2022-10-17 14:28:45 -0400

    Add verbose condition block for constant def
    Signed-off-by: Akash Manohar <akash@akash.im>

--- src/utils/embedded_file_manager.cr
+++ src/utils/embedded_file_manager.cr
@@ -1,5 +1,9 @@
 module EmbeddedFileManager
   macro cluster_tools
-    CLUSTER_TOOLS = ENV["USE_MANIFEST_SPECS_DIRECTORY"]? ? File.read("./tools/cluster-tools/manifest.yml") : File.read("./lib/cluster_tools/tools/cluster-tools/manifest.yml")
+    CLUSTER_TOOLS = if ENV["USE_MANIFEST_SPECS_DIRECTORY"]?
+        File.read("./tools/cluster-tools/manifest.yml")
+      else
+        File.read("./lib/cluster_tools/tools/cluster-tools/manifest.yml")
+      end
   end
 end
