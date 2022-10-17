8f5bfebaa44ec2e011f83d13af721f4b61015f9a
Akash Manohar <akash@akash.im>
2022-10-17 14:11:35 -0400

    Rename env var to USE_MANIFEST_SPECS_DIRECTORY
    Signed-off-by: Akash Manohar <akash@akash.im>

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,5 +4,5 @@ require "colorize"
 ENV["CRYSTAL_ENV"] = "TEST"
 
 # This forces use of cluster-tools manifest in the repo's tools dir when running specs.
-ENV["USE_MANIFEST_FOR_SPECS"] = "yes"
+ENV["USE_MANIFEST_SPECS_DIRECTORY"] = "yes"
 
--- src/utils/embedded_file_manager.cr
+++ src/utils/embedded_file_manager.cr
@@ -1,5 +1,5 @@
 module EmbeddedFileManager
   macro cluster_tools
-    CLUSTER_TOOLS = ENV["USE_MANIFEST_FOR_SPECS"]? ? File.read("./tools/cluster-tools/manifest.yml") : File.read("./lib/cluster_tools/tools/cluster-tools/manifest.yml")
+    CLUSTER_TOOLS = ENV["USE_MANIFEST_SPECS_DIRECTORY"]? ? File.read("./tools/cluster-tools/manifest.yml") : File.read("./lib/cluster_tools/tools/cluster-tools/manifest.yml")
   end
 end
